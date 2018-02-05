38c52167cfdf93ede058a78a0cd718f93e490c7b
dhruv chauhan <dc14690@my.bristol.ac.uk>
2018-02-05 15:30:20 +0000

    working on sending float to pure data

---
+++ Assets/Scripts/oscControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using LibPDBinding;
+
+public class oscControl : MonoBehaviour {
+
+	public float frequency;
+	
+	// Update is called once per frame
+	void Update () {
+
+		LibPD.SendFloat ("frequency", frequency);
+	}
+}
