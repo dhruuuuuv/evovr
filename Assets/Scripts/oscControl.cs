caa779d5b4ff66e7d1372e20fb4bd833af7beb23
Dhruv Chauhan <dc14690@bristol.ac.uk>
2018-02-07 17:46:30 +0000

    added cube that mods frequency

--- Assets/Scripts/oscControl.cs
+++ Assets/Scripts/oscControl.cs
@@ -6,11 +6,19 @@ using LibPDBinding;
 
 public class oscControl : MonoBehaviour {
 
-	public float frequency;
-	
+	public Rigidbody rb;
+	public float freq_factor = 100;
+
+	void Start()
+	{
+	}
+
 	// Update is called once per frame
 	void Update () {
-
+		Vector3 cube_pos = rb.position;
+		float frequency = cube_pos.x * freq_factor;
+		Debug.Log (cube_pos.x);
+		Debug.Log (frequency);
 		LibPD.SendFloat ("frequency", frequency);
 	}
 }
