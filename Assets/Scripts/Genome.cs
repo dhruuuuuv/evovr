7881b19fdacf8ac4a2c121953c1a0d6616145b65
dhruv chauhan <dc14690@my.bristol.ac.uk>
2018-02-19 16:04:43 +0000

    reworked files into genome, and added possible properties and pd receives

---
+++ Assets/Scripts/Genome.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Genome : MonoBehaviour {
+
+//	public poly_object ... ?
+
+//	public interaction ... ?
+
+//	pure data receiver index of array
+	public int pd_receiver_index;
+
+//	rigidbody property index float
+	public int rb_property_index;
+
+//	object to create and play with
+	public Rigidbody rb;
+
+//	List of property and floats
+	List<float> rb_prop;
+
+	public string[] sound_receives = {"lfo-freq",
+		"sd-l",
+		"sd-fb",
+		"sd-lp",
+		"sd-r",
+		"ahr-lfo-1-a",
+		"ahr-lfo-1-r",
+		"ahr-lfo-1-h",
+		"ahr-lfo-2-a",
+		"ahr-lfo-2-r",
+		"ahr-lfo-2-h",
+		"vca-1",
+		"vca-2",
+		"seq-scale",
+		"glob-clk-rate"
+	};
+
+	public Genome() {
+		get_rb_properties ();
+
+		pd_receiver_index = Random.Range (0, sound_receives.Length);
+		rb_property_index = Random.Range (0, rb_prop.Length);
+	}
+
+	public Genome(int pdri, int rbpi) {
+		get_rb_properties ();
+
+		pd_receiver_index = pdri;
+		rb_property_index = rbpi;
+	}
+
+
+	float get_property_float () {
+		return rb_property_index [rb_property_index];
+	}
+
+	string get_pd_string () {
+		return sound_receives [pd_receiver_index];
+	}
+		
+	//	get properties of rigidbody and store them in an array
+	void get_rb_properties() {
+
+		rb_prop = new List<float>();
+
+		//		get the x, y, z position and the magnitude of the vector
+		rb_prop.Add(rb.position.x);
+		rb_prop.Add(rb.position.y);
+		rb_prop.Add(rb.position.z);
+		rb_prop.Add(rb.position.magnitude);
+
+		//		get the euler x, y, z rotation and the magnitude of the resultant vector.
+		rb_prop.Add(rb.rotation.eulerAngles.x);
+		rb_prop.Add(rb.rotation.eulerAngles.y);
+		rb_prop.Add(rb.rotation.eulerAngles.z);
+		rb_prop.Add(rb.rotation.eulerAngles.magnitude);
+
+		//		get the velocity x, y, z components, and the magnitude of the vector
+		rb_prop.Add(rb.velocity.x);
+		rb_prop.Add(rb.velocity.y);
+		rb_prop.Add(rb.velocity.z);
+		rb_prop.Add(rb.velocity.magnitude);
+	}
+		
+
+//	//	gets a random element from the sound receives array (ie. what to send to pure data)
+//	string get_random_sound_receive() {
+//		return sound_receives[Random.Range(0,sound_receives.Length)];
+//	}
+}
--- Assets/Scripts/oscControl.cs
+++ Assets/Scripts/oscControl.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Reflection;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,30 +10,83 @@ public class oscControl : MonoBehaviour {
 	public Rigidbody rb;
 	public float freq_factor = 100;
 
+	public float min_x_lim = -30;
+	public float max_x_lim = 30;
+
+	List<float> rb_prop;
+
+	public string[] sound_receives = {"lfo-freq",
+		"sd-l",
+		"sd-fb",
+		"sd-lp",
+		"sd-r",
+		"ahr-lfo-1-a",
+		"ahr-lfo-1-r",
+		"ahr-lfo-1-h",
+		"ahr-lfo-2-a",
+		"ahr-lfo-2-r",
+		"ahr-lfo-2-h",
+		"vca-1",
+		"vca-2",
+		"seq-scale",
+		"glob-clk-rate"
+	};
+
 	void Start()
 	{
+		rb_prop = new List<float>();
+		get_rb_properties ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		float frequency = get_inst_pos ();
-		Debug.Log (frequency);
+//		Debug.Log (frequency);
 		LibPD.SendFloat ("frequency", frequency);
 	}
 
+//	gets the x value of the rb and maps it to a frequency range.
 	float get_inst_pos() {
 		Vector3 inst_pos = rb.position;
 		float frequency = inst_pos.x;
 
 
-		float mapped_frequency = remap (frequency, -30, 30, (float) -0.25, (float) 0.25);
+		float mapped_frequency = remap (frequency, min_x_lim, max_x_lim, (float) -0.25, (float) 0.25);
 		return mapped_frequency;
 	}
 
-
+//	scales a value (s) from original range (a1, a2) to new range (b1, b2)
 	float remap(float s, float a1, float a2, float b1, float b2)
 	{
 		return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
 	}
 
+	//	gets a random element from the sound receives array (ie. what to send to pure data)
+	string get_random_sound_receive() {
+		return sound_receives[Random.Range(0,sound_receives.Length)];
+	}
+
+//	get properties of rigidbody and store them in an array
+	void get_rb_properties() {
+
+//		get the x, y, z position and the magnitude of the vector
+		rb_prop.Add(rb.position.x);
+		rb_prop.Add(rb.position.y);
+		rb_prop.Add(rb.position.z);
+		rb_prop.Add(rb.position.magnitude);
+
+//		get the euler x, y, z rotation and the magnitude of the resultant vector.
+		rb_prop.Add(rb.rotation.eulerAngles.x);
+		rb_prop.Add(rb.rotation.eulerAngles.y);
+		rb_prop.Add(rb.rotation.eulerAngles.z);
+		rb_prop.Add(rb.rotation.eulerAngles.magnitude);
+
+//		get the velocity x, y, z components, and the magnitude of the vector
+		rb_prop.Add(rb.velocity.x);
+		rb_prop.Add(rb.velocity.y);
+		rb_prop.Add(rb.velocity.z);
+		rb_prop.Add(rb.velocity.magnitude);
+	}
+
 }
+	
