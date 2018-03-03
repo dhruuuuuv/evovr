510fb41b3bff7a7b7df79ae73267e4db72c01051
dhruv chauhan <dc14690@my.bristol.ac.uk>
2018-03-03 14:47:08 +0000

    tried heavy but automatonism doesn't want to compile with it

---
+++ Assets/Scripts/myAudioLibController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myAudioLibController : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent<Rigidbody> ();
+
+		Hv_test_1_AudioLib script = GetComponent<Hv_test_1_AudioLib> ();
+
+		// get parameter
+		float freq = script.GetFloatParameter(Hv_test_1_AudioLib.Parameter.Freq);
+
+		float x = rb.position.x * 100;
+
+		//set parameter
+		script.SetFloatParameter(Hv_test_1_AudioLib.Parameter.Freq, x);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Hv_test_1_AudioLib script = GetComponent<Hv_test_1_AudioLib> ();
+
+		// get parameter
+		float freq = script.GetFloatParameter(Hv_test_1_AudioLib.Parameter.Freq);
+
+		float x = rb.position.x * 100;
+
+		//set parameter
+		script.SetFloatParameter(Hv_test_1_AudioLib.Parameter.Freq, x);
+	}
+}
